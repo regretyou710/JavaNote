d740e181e1a81f813c5567770f8119479eab24ff
eason <regretyou710@gmail.com>
2023-12-16 08:28:36 +0800

    202312160828

--- 模塊化/space5/zoo.animal.shows/src/module-info.java
+++ 模塊化/space5/zoo.animal.shows/src/module-info.java
@@ -2,6 +2,7 @@ module zoo.animal.shows {
     exports zoo.animal.shows.content;
     exports zoo.animal.shows.media;
     exports zoo.animal.shows.schedule;
-//    requires zoo.animal.feeding;
+//    requires zoo.animal.feeding;//因為zoo.animal.care中requires transitive zoo.animal.feeding，所以不用再引入
+//    requires zoo.animal.care;
     requires transitive zoo.animal.care;
 }
--- 模塊化/space5/zoo.staff/src/module-info.java
+++ 模塊化/space5/zoo.staff/src/module-info.java
@@ -1,5 +1,5 @@
 module zoo.staff {
-//    requires zoo.animal.feeding;
-//    requires zoo.animal.care;
+//    requires zoo.animal.feeding;//因為zoo.animal.care中requires transitive zoo.animal.feeding，所以不用再引入
+//    requires zoo.animal.care;//因為zoo.animal.shows中requires transitive zoo.animal.care，所以不用再引入
     requires zoo.animal.shows;
 }
